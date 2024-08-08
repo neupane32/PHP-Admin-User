4410ba5427deeff7a801bc7b9f2f631534826cb6
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-08 15:53:04 +0545

    create user css style

--- views/create.php
+++ views/create.php
@@ -1,17 +1,23 @@
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
+    <link rel="stylesheet" href="../css/cretaeStyle.css">
 </head>
 <body>
-    <h1>Create a User</h1>
-    <form action="/PHP-ADMIN-USER/?action=store" method="post">
-        <label>Name:</label>
-        <input type="text" name="name" required><br><br>
-        <label>Email:</label>
-        <input type="email" name="email" required><br><br>
-        <button type="submit">Create</button>
-    </form>
+    <div class="container">
+        <h1>Create a User</h1>
+        <form action="/PHP-ADMIN-USER/?action=store" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <button type="submit">Create</button>
+        </form>
+    </div>
 </body>
 </html>
