2f5a5d68c3895f1ca55e0b25002c553103e5c9c5
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-08 15:58:14 +0545

    dashboard i.e. main page

--- views/dashboard.php
+++ views/dashboard.php
@@ -6,42 +6,45 @@ if (!isset($_SESSION['admin'])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
+    <link rel="stylesheet" href="../css/dashboardStyle.css">
 </head>
 <body>
-    <h1>Users</h1>
-    <a href="/PHP-ADMIN-USER/?action=create">Create User</a>
-    <hr>
-    <table border="1" cellpadding="10" cellspacing="0">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if (!empty($users)): ?>
-                <?php foreach ($users as $user): ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($user['name']); ?></td>
-                        <td><?php echo htmlspecialchars($user['email']); ?></td>
-                        <td>
-                            <a href="/PHP-ADMIN-USER/?action=edit&id=<?php echo $user['id']; ?>">Edit</a>
-                            <a href="/PHP-ADMIN-USER/?action=delete&id=<?php echo $user['id']; ?>">Delete</a>
-                            <a href="">Add Product</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
+    <div class="container">
+        <h1>Users</h1>
+        <a href="/PHP-ADMIN-USER/?action=create" class="create-user">Create User</a>
+        <table>
+            <thead>
                 <tr>
-                    <td colspan="3">No users found</td>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
                 </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php if (!empty($users)): ?>
+                    <?php foreach ($users as $user): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($user['name']); ?></td>
+                            <td><?php echo htmlspecialchars($user['email']); ?></td>
+                            <td>
+                                <a href="/PHP-ADMIN-USER/?action=edit&id=<?php echo $user['id']; ?>">Edit</a>
+                                <a href="/PHP-ADMIN-USER/?action=delete&id=<?php echo $user['id']; ?>" class="delete">Delete</a>
+                                <a href="#">Add Product</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="3" class="no-users">No users found</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
-
