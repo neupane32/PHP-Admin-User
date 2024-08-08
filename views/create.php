ba90ede74897557cfa91951a5758df0c4ffd101a
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-08 15:55:35 +0545

    create page for user

--- views/create.php
+++ views/create.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
-    <link rel="stylesheet" href="../css/cretaeStyle.css">
+    <link rel="stylesheet" href="../css/createStyle.css">
 </head>
 <body>
     <div class="container">
